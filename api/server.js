54c4e12d6f4c1e022675905757eb34604111caae
jgk98f <jgk98f@umsl.edu>
2017-03-13 22:00:02 -0500

    Revised API for client team
    API now no longer supports delete and update. New functionality to
    search for frameworks by business type such as medical, default, pcidss,
    etc...

--- api/server.js
+++ api/server.js
@@ -2,23 +2,29 @@ var express = require('express'),
 framework = require('./routes/api');
 
 var app = express();
+var morgan = require('morgan')
 
-app.configure(function () {
-    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser());
-});
+// parse urlencoded request bodies into req.body
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan('combined'))
 
-app.get('/api/frameworks', framework.findAll);
-app.get('/api/frameworks/:id', framework.findById);
-app.post('/api/frameworks', framework.addFramework);
-app.put('/api/frameworks/:id', framework.updateFrameworkById);
-app.delete('/api/frameworks/:id', framework.deleteFrameworkById);
 
-app.get('/api/frameworks/:id/controls', framework.findAllFrameworkControls);
-app.get('/api/frameworks/:id/controls/:name', framework.findFrameworkControlByIdAndName);
-app.post('/api/frameworks/:id/controls', framework.addFrameworkControl);
-app.put('/api/frameworks/:id/controls/:name', framework.updateFrameworkControlByIdAndName);
-app.delete('/api/frameworks/:id/controls/:name', framework.deleteFrameworkControlsByIdAndName);
+app.get('/api/frameworks', framework.findAllFrameworks);
+app.get('/api/frameworks/:name', framework.findFrameworkByName);
+app.get('/api/frameworks/type/:type', framework.findFrameworkByBusinessType);
+
+app.get('/api/frameworks/:name/controls', framework.findAllFrameworkControls);
+app.get('/api/frameworks/:fname/controls/:name', framework.findFrameworkControlByFrameworkNameAndControlName);
+
+
 
 app.listen(3000);
 console.log('Listening on port 3000...');
+
+/*
+	Need to npm install:
+  	npm install connect
+  	npm install morgan
+  	npm install body-parser
+*/
