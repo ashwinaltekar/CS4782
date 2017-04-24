deb24a586650a8f3c0b2996ccf6d8ea7dc66aeaa
trokair <wandos1@yahoo.com>
2017-04-23 21:05:11 -0500

    Added Basic Login
    Added the base functionality for a username and password login. It does
    not use sessions yet and it does not pull passwords and usernames from
    the database.

--- api/server.js
+++ api/server.js
@@ -8,6 +8,25 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan('combined'))
 
+var passport = require('passport');  
+app.use (passport.initialize());
+
+var LocalStrategy = require('passport-local').Strategy;
+var flash = require ("connect-flash");
+app.use (flash());
+
+//tell passport to user local strategy which is 
+//password and username login 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+	  //not using database access right now
+	  if (username == "test" && password == "test")
+		  done (null, "yes");
+	  else
+		  done (null, false);
+  }
+));
+
 app.get('/api/frameworks', framework.findAllFrameworks);
 app.get('/api/frameworks/:name', framework.findFrameworkByName);
 app.get('/api/frameworks/type/:type', framework.findFrameworkByBusinessType);
@@ -18,6 +37,12 @@ app.get('/api/references/:name', framework.findControlsByReference);
 app.get('/api/frameworks/controls/like/:description', framework.findSimilarControlsByDescription);
 app.get('/api/frameworks/controls/:control/tag/:tag', framework.addTagToControl);
 app.get('/api/frameworks/controls/tag/:tag', framework.findControlsByTag);
+
+
+app.post('/api/login',
+  passport.authenticate('local', { successRedirect: '/api/sucess',
+  failureRedirect: '/api/failure', session: false})
+);
 
 app.listen(3000);
 console.log('Listening on port 3000...');
